3758955fcb5044facc3cc4644727c8579d6057f4
Sid <tech.sidhanta@gmail.com>
2016-08-23 17:59:19 +0530

    some changes done..
    Signed-off-by: Sid <tech.sidhanta@gmail.com>

--- src/com/mlabs/test/Triangle1.java
+++ src/com/mlabs/test/Triangle1.java
@@ -13,7 +13,7 @@ public class Triangle1 {
 		 *****
 		 */
 		
-		System.out.println("This is a Triangle.");
+		System.out.println("This is a Triangle..");
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
 				if (j <= i) {
